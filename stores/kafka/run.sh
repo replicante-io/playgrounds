d1253ffbdcdfc1f373b6189f70d1b4c61c21fcf7
Stefano Pogliani <stefano@spogliani.net>
2020-06-22 21:19:13 +0100

    Update replicante versions and fix quick start bugs

--- stores/kafka/run.sh
+++ stores/kafka/run.sh
@@ -2,7 +2,7 @@
 
 # Inject CLUSTER_ID into agent config.
 cat /opt/replicante/agent-kafka.template.yaml \
-  | sed "s/'INJECTED_CLUSTER_ID'/'${CLUSTER_ID}'/" \
+  | sed "s/INJECTED_CLUSTER_ID/${CLUSTER_ID}/" \
   > /home/replicante/agent-kafka.yaml
 
 # Start the Replicante Agent.
